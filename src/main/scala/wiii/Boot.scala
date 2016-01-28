0a2ea9d9ab0a93b5d02a92ab5e834e71f02ae76d
John Wass <jwass3@gmail.com>
2016-01-28 00:55:20 -0500

    initial file access

---
+++ src/main/scala/wiii/Boot.scala
@@ -0,0 +1,60 @@
+package wiii
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.HttpEntity.Chunked
+import akka.http.scaladsl.model.{ContentTypes, HttpResponse}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
+import wiii.awa.WebApi
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+/**
+ * a hand rolled httpfs like interface to hdfs
+ */
+object Boot extends App with WebApi with LazyLogging {
+    implicit val actorSystem: ActorSystem = ActorSystem("HDFSService")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    override def config: Option[Config] = Option(actorSystem.settings.config)
+
+    val download =
+        (get & pathPrefix("dl")) {
+            path("file" / Segment) { path =>
+                complete {
+                    val source = Source.actorPublisher(StreamingActor.props("localhost", 5000, path))
+                    HttpResponse(entity = Chunked(ContentTypes.`application/octet-stream`, source))
+                }
+            } ~ path("dir" / Segment) { path =>
+                complete(s"read directory at $path")
+            }
+        }
+
+    val upload =
+        (put & path("ul" / Segment)) { path =>
+            complete("stream into hdfs")
+        }
+
+    val delete =
+        (get & pathPrefix("del")) {
+            path("file" / Segment) { path =>
+                complete(s"delete file $path")
+            } ~ path("dir" / Segment) { path =>
+                complete(s"delete dir $path")
+            }
+        }
+
+    val stats =
+        (get & pathPrefix("stats")) {
+            complete("stats request")
+        }
+
+
+    logger.info("starting Service")
+    webstart(download ~ upload ~ delete ~ stats)
+
+    Await.ready(actorSystem.whenTerminated, Duration.Inf)
+}
---
+++ src/main/scala/wiii/FilesystemActor.scala
@@ -0,0 +1,43 @@
+package wiii
+
+import java.io.FileNotFoundException
+
+import akka.actor.{Actor, Props}
+import akka.stream.actor.ActorPublisher
+import akka.stream.actor.ActorPublisherMessage.{Request, Cancel}
+import akka.stream.scaladsl.StreamConverters
+import akka.util.ByteString
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+
+case class LoadFile(path: String)
+
+object StreamingActor {
+    def props(host: String, port: Int, path: String) = Props(new StreamingActor(host, port, path))
+}
+class StreamingActor(host: String, port: Int, path: String) extends ActorPublisher[ByteString]  with LazyLogging {
+    val filesys = {
+        val conf = new Configuration()
+        conf.set("fs.default.name", s"hdfs://$host:$port")
+        FileSystem.get(conf)
+    }
+
+    val chunkSize = 1024
+    val arr = Array.ofDim[Byte](chunkSize)
+
+    def receive: Receive = {
+        case Request(cnt) =>
+            val uri = new Path(path)
+            uri match {
+                case p if !filesys.exists(p) => throw new FileNotFoundException(s"$p does not exist")
+                case p if !filesys.getFileStatus(p).isFile => throw new FileNotFoundException(s"$p is not a file")
+                case p =>
+                    val is = filesys.open(p)
+                    val readBytes = is.read(arr)
+                    onNext(ByteString.fromArray(arr, 0, readBytes))
+            }
+        case Cancel => context.stop(self)
+        case _ =>
+    }
+}
